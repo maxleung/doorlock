280ad0d3d4220c717411528e39cd7246ef7d4dec
max <no@no.com>
2018-05-03 16:11:31 +0800

    reg

---
+++ DoorLock/RegViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegViewController.h
+//  DoorLock
+//
+//  Created by max leung on 3/5/2018.
+//  Copyright © 2018 TM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegViewController : UIViewController
+
+@end
