0debebdf31c75b07480d5d9a8cc82e1634484375
max <no@no.com>
2018-05-09 18:27:00 +0800

    md

---
+++ DoorLock/MyInfo.h
@@ -0,0 +1,13 @@
+//
+//  MyInfo.h
+//  DoorLock
+//
+//  Created by max leung on 9/5/2018.
+//  Copyright © 2018 TM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyInfo : UIViewController
+- (IBAction)PopToMyLock:(id)sender;
+@end
