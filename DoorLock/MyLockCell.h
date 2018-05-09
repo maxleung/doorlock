18848c10839e30f732a5c1c993ce3fffc6e045b9
max <no@no.com>
2018-05-09 15:21:25 +0800

    mylockcell

---
+++ DoorLock/MyLockCell.h
@@ -0,0 +1,13 @@
+//
+//  MyLockCell.h
+//  DoorLock
+//
+//  Created by max leung on 9/5/2018.
+//  Copyright © 2018 TM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyLockCell : UITableViewCell
+
+@end
---
+++ DoorLock/MyLockCell.m
@@ -0,0 +1,24 @@
+//
+//  MyLockCell.m
+//  DoorLock
+//
+//  Created by max leung on 9/5/2018.
+//  Copyright © 2018 TM. All rights reserved.
+//
+
+#import "MyLockCell.h"
+
+@implementation MyLockCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
