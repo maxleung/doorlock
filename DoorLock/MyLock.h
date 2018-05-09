f265febbf75d80ec313e293c146294667d9219c9
max <no@no.com>
2018-05-09 15:19:25 +0800

    mylock

---
+++ DoorLock/MyLock.h
@@ -0,0 +1,13 @@
+//
+//  MyLock.h
+//  DoorLock
+//
+//  Created by max leung on 9/5/2018.
+//  Copyright © 2018 TM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyLock : UIViewController
+
+@end
---
+++ DoorLock/MyLock.m
@@ -0,0 +1,37 @@
+//
+//  MyLock.m
+//  DoorLock
+//
+//  Created by max leung on 9/5/2018.
+//  Copyright © 2018 TM. All rights reserved.
+//
+
+#import "MyLock.h"
+
+@interface MyLock ()
+
+@end
+
+@implementation MyLock
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
