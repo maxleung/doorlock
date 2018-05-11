dd1cd5b1a56558caa2d56588c81fa97aaad54a77
max <no@no.com>
2018-05-11 16:18:49 +0800

    password

---
+++ DoorLock/Password/AddPassword.h
@@ -0,0 +1,13 @@
+//
+//  AddPassword.h
+//  DoorLock
+//
+//  Created by max leung on 11/5/2018.
+//  Copyright © 2018 TM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddPassword : UIViewController
+
+@end
